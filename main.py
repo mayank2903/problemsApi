242cc9e34911def8797936698ba9c0a95e3f3e3e
mayank2903 <pc.mayank@gmail.com>
2023-10-26 12:04:40 +0530

    pivot - realized that my focus should first be evaluation server (original idea). problems are just html files which are cheap.

--- main.py
+++ main.py
@@ -4,83 +4,52 @@ import uuid
 app = Flask(__name__)
 app.debug = True
 
-problems = {
-    '1': {
-        'id': '1',
-        'statement': 'problem statement contents',
-        'test_cases': ['test case1', 'testcase2'],
-        'solutions': ['solution1', 'solution2'],
-    },
-    '2': {
-        'id': '2',
-        'statement': 'problem statement contents',
-        'test_cases': ['test case1', 'testcase2'],
-        'solutions': ['solution1', 'solution2'],
-    },
-    '3': {
-        'id': '3',
-        'statement': 'problem statement contents',
-        'test_cases': ['test case1', 'testcase2'],
-        'solutions': ['solution1', 'solution2'],
-    }
-}
+pass_result = "PASS"
+fail_result = "FAIL"
 
 
 @app.route('/', methods=['GET'])
 def hello():
     data = 'hello'
-    return jsonify({'data': data})
+    return jsonify({'hello': 'world'})
 
 
-@app.route('/problems/<string:problem_id>', methods=['GET'])
-def get_method(problem_id):
-    # todo: optimize below for faster lookup.
-    if problem_id in problems.keys():
-        return jsonify(problems[problem_id])
-    else:
-        return {}
+def evaluate_based_on_language(code, test_cases, solutions, language):
+    match language:
+        case "c":
+            return evaluate_c_submission()
+        case "cpp":
+            return evaluate_cpp_submission()
+        case "java":
+            return evaluate_java_submission()
+        case "python":
+            return evaluate_py_submission()
+        case default:
+            pass
+    return None
 
 
-@app.route('/problems/', methods=['GET'])
-def list_method():
-    return jsonify(problems)
+def evaluate_c_submission():
+    return pass_result
 
 
-@app.route('/problems', methods=['POST'])
-def insert_method():
-    statement = request.form['statement']
+def evaluate_cpp_submission():
+    return pass_result
+
+
+def evaluate_java_submission():
+    return pass_result
+
+
+def evaluate_py_submission():
+    return pass_result
+
+
+@app.route('/submissions/evaluate', methods=['POST'])
+def evaluate():
+    code = request.form['code']
     test_cases = request.form['test_cases']
     solutions = request.form['solutions']
-    id = str(uuid.uuid4())
-    problems[id] = {
-        'id': id,
-        'statement': statement,
-        'test_cases': test_cases,
-        'solutions': solutions
-    }
-    return jsonify(problems[id])
-
-
-@app.route('/problems/<string:problem_id>', methods=['PATCH'])
-def patch_method(problem_id):
-    if problem_id not in problems.keys():
-        # throw 404 or something
-        return {}
-
-    updated_problem = problems[problem_id]
-    if 'statement' in request.form.keys():
-        updated_problem['statement'] = request.form['statement']
-    if 'test_cases' in request.form.keys():
-        updated_problem['test_cases'] = request.form['test_cases']
-    if 'solutions' in request.form.keys():
-        updated_problem['solutions'] = request.form['solutions']
-
-    problems[problem_id]=updated_problem
-    return jsonify(updated_problem)
-
-
-@app.route('/problems/<string:problem_id>', methods=['DELETE'])
-def delete_method(problem_id):
-    del problems[problem_id]
-    return Response(status=204)
-
+    language = request.form['language']
+    result = evaluate_based_on_language(code, test_cases, solutions, language)
+    return jsonify({"result": result})
